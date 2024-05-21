7c14d19216e44a7beee6441e2cff75b5509649f8
Gopall <mnaufalalfirdaus1703@gmail.com>
2024-05-21 14:02:54 +0700

    w

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,9 +29,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    buildFeatures{
-        viewBinding = true
-    }
 }
 
 dependencies {
@@ -42,4 +39,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("com.etebarian:meow-bottom-navigation-java:1.2.0")
 }
